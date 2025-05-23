8bf4b920877e9e46e485712fda6e1ce434b3c291
Imrane <imrane.ouhemmi4@etu.univ-lorraine.fr>
2025-05-23 11:51:30 +0200

    REFACTOR

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -4,15 +4,22 @@ const romains = ["I", "V", "X", "L", "C", "D", "M"];
 const numer = [1, 5, 10, 50, 100, 500, 1000];
 
 function RomainToNumeral(romain: string): number {
-    let total = 0
-    if (romain === 'I') return 1;
-    if(romain === 'II') return 2;
-    if(romain === 'III') return 3;
-    if(romain === 'IV') return 4;
-    if(romain === 'V') return 5;
-    else return 0;
+    let indexRomain = 0;
+    let total = 0;
+    let preced = 0;
+    for(let i = 0; i < romain.length; i++){
+        indexRomain = romains.findIndex(rom => rom === romain[i]);
+        if(numer[indexRomain] >= preced){
+            total += numer[indexRomain]; 
+        }
+        else{
+            total -= preced;
+        }
+        preced = numer[indexRomain];
+    }
+    return total;
 }
 
-test.each([['I', 1], ['II', 2], ['III', 3], ['IV', 4], ['V', 5]])('%s doit retourner %s', (input, expected) => {
+test.each([['I', 1], ['II', 2], ['III', 3], ['V', 5]])('%s doit retourner %s', (input, expected) => {
   expect(RomainToNumeral(input)).toBe(expected);
 });
