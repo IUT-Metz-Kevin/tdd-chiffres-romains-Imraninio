647ebc65a2af28a3acd88cf33e605b42a15ee76f
Imrane <imrane.ouhemmi4@etu.univ-lorraine.fr>
2025-05-23 10:53:43 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,5 @@
-// Write your test here
+import { it, expect } from "vitest";
+
+it("Doit retourner 1 lorsqu'on entre le chiffre romain I.", function () {
+  expect(RomainToNumeral("I")).toBe(1);
+});
