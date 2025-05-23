ff39d2a997ce830a319792c09f0b794e5752db8a
Imrane <imrane.ouhemmi4@etu.univ-lorraine.fr>
2025-05-23 11:15:17 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,11 +1,14 @@
 import { it, expect } from 'vitest';
 
+const romains = ["I", "V", "X", "L", "C", "D", "M"];
+const numer = [1, 5, 10, 50, 100, 500, 1000];
+
 function RomainToNumeral(romain: string): number {
   if (romain === 'I') return 1;
   if(romain === 'II') return 2;
   else return 0;
 }
 
-test.each([['I', 1], ['II', 2]])('%s doit retourner %s', (input, expected) => {
+test.each([['I', 1], ['II', 2], ['III', 3]])('%s doit retourner %s', (input, expected) => {
   expect(RomainToNumeral(input)).toBe(expected);
 });
